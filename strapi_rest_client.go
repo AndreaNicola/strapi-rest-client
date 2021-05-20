fcb03fd31680700940ca342dd33f6fcd9a79d73e
Andrea Nicola <cico.85@gmail.com>
2021-05-20 14:46:15 +0200

    new error handling

--- strapi_rest_client.go
+++ strapi_rest_client.go
@@ -13,7 +13,6 @@ type StrapiRestClient struct {
 	BaseUrl string
 }
 
-
 func New() StrapiRestClient {
 	return NewWithUlr(os.Getenv("STRAPI_BASE_URL"))
 }
@@ -31,7 +30,7 @@ func NewWithUlr(baseUrl string) (src StrapiRestClient) {
 
 type callback func(map[string]interface{})
 
-func call(method, url string, callback callback) {
+func call(method, url string, callback callback) error {
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
@@ -42,40 +41,42 @@ func call(method, url string, callback callback) {
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	var bodyMap map[string]interface{}
 	err = json.Unmarshal(body, &bodyMap)
 
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	callback(bodyMap)
 
+	return nil
+
 }
 
-func (src StrapiRestClient) GetUser(id int) *StrapiUser {
+func (src StrapiRestClient) GetUser(id int) (*StrapiUser, error) {
 	var result StrapiUser
-	call("GET", src.BaseUrl+"/users/"+strconv.Itoa(id), result.New)
-	return &result
+	err := call("GET", src.BaseUrl+"/users/"+strconv.Itoa(id), result.New)
+	return &result, err
 }
 
-func (src StrapiRestClient) GetProduct(id int) *StrapiProduct {
+func (src StrapiRestClient) GetProduct(id int) (*StrapiProduct, error) {
 	var result StrapiProduct
-	call("GET", src.BaseUrl+"/products/"+strconv.Itoa(id), result.New)
-	return &result
+	err := call("GET", src.BaseUrl+"/products/"+strconv.Itoa(id), result.New)
+	return &result, err
 }
